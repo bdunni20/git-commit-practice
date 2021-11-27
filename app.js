a7ea19bde28f5aa4e310af6f3fcdc863ea0060cd
Brian <briandunnigan20@gmail.com>
2021-11-26 22:30:41 -0800

    added page structure and made all the links work

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("website running");
+
+function toggleHide() {
+  const hiddenText = document.querySelector('.hiddenText');
+  if (hiddenText.style.visibility === 'inherit') {
+    hiddenText.style.visibility = 'hidden';
+  }else {
+    hiddenText.style.visibility = 'inherit';
+  }
+}
+
+
+const hideAtcl = document.querySelector('.hiddenButton');
+
+hideAtcl.addEventListener('click', toggleHide);
